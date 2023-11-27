99511bc14f4b15f5e7241cf0efaa2bd29dab2ba1
albanecoiffe <90681785+albanecoiffe@users.noreply.github.com>
2023-11-27 18:43:38 -0500

    Add files via upload

---
+++ data/data.js
@@ -0,0 +1,13 @@
+const data = [
+    {
+        title: "Hedgehog",
+        def: "The European hedgehog is a small mammal that can measure up to 32 centimeters long (from nose to tail) and weigh between 450g and 700gr.",
+        img: "https://www.wanimo.com/veterinaire/wp-content/uploads/2018/05/images_articles_herisson.jpg"
+    },
+    {
+        title: "Hobby horse",
+        def: "A hobby horse (or hobby-horse) is a child's toy horse. Children played at riding a wooden hobby horse made of a straight stick with a small horse's head (of wood or stuffed fabric), and perhaps reins, attached to one end.",
+        img: "https://i.etsystatic.com/22670774/r/il/c10ef4/2676252703/il_fullxfull.2676252703_o7zp.jpg"
+    },
+    // Add more objects as needed
+];
