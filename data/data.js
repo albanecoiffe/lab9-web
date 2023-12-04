b09a6f03641196bc7505f02b85ade03813704ab0
Maelwennlbdr <101875283+Maelwennlbdr@users.noreply.github.com>
2023-12-04 16:24:35 +0100

    Update data.js

--- data/data.js
+++ data/data.js
@@ -12,16 +12,16 @@ const data = [
     {
         title: "Red Panda",
         def: "The red panda is a mammal native to the eastern Himalayas and southwestern China. It is listed as Endangered on the IUCN Red List because the wild population is estimated at fewer than 10,000 mature individuals and continues to decline due to habitat loss and fragmentation.",
-        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Ailurus_fulgens_-_Sadiya_Adam.jpg/800px-Ailurus_fulgens_-_Sadiya_Adam.jpg"
+        img: "https://facts.net/wp-content/uploads/2020/08/AdobeStock_209028852.jpeg"
     },
     {
         title: "Elephant",
         def: "Elephants are large mammals of the family Elephantidae and the order Proboscidea. Three species are currently recognized: the African bush elephant, the African forest elephant, and the Asian elephant.",
-        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/African_Bush_Elephant.jpg/800px-African_Bush_Elephant.jpg"
+        img: "https://1.bp.blogspot.com/-B1qPgqddbsE/XzfS3q4b9hI/AAAAAAAAAN0/ZBujuubx-_ElUaMOx75ECYVBGgWfBidvgCLcBGAsYHQ/s2048/elephant+2.jpg"
     },
     {
         title: "Dolphin",
         def: "Dolphins are highly intelligent marine mammals and are part of the toothed whales family. They are known for their playful behavior and complex communication skills.",
-        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Bottlenose_Dolphin_%28South_Carolina_Aquarium%29.jpg/800px-Bottlenose_Dolphin_%28South_Carolina_Aquarium%29.jpg"
+        img: "https://cdn.britannica.com/02/6602-050-E4B2B3A9/Bottlenose-dolphins.jpg"
     },
 ];
