ca6a6f74f905a0e95638158b7fe496815d199c6b
Maelwennlbdr <101875283+Maelwennlbdr@users.noreply.github.com>
2023-12-04 16:22:47 +0100

    Update data.js

--- data/data.js
+++ data/data.js
@@ -9,5 +9,19 @@ const data = [
         def: "A hobby horse (or hobby-horse) is a child's toy horse. Children played at riding a wooden hobby horse made of a straight stick with a small horse's head (of wood or stuffed fabric), and perhaps reins, attached to one end.",
         img: "https://i.etsystatic.com/22670774/r/il/c10ef4/2676252703/il_fullxfull.2676252703_o7zp.jpg"
     },
-    // Add more objects as needed
+    {
+        title: "Red Panda",
+        def: "The red panda is a mammal native to the eastern Himalayas and southwestern China. It is listed as Endangered on the IUCN Red List because the wild population is estimated at fewer than 10,000 mature individuals and continues to decline due to habitat loss and fragmentation.",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Ailurus_fulgens_-_Sadiya_Adam.jpg/800px-Ailurus_fulgens_-_Sadiya_Adam.jpg"
+    },
+    {
+        title: "Elephant",
+        def: "Elephants are large mammals of the family Elephantidae and the order Proboscidea. Three species are currently recognized: the African bush elephant, the African forest elephant, and the Asian elephant.",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/African_Bush_Elephant.jpg/800px-African_Bush_Elephant.jpg"
+    },
+    {
+        title: "Dolphin",
+        def: "Dolphins are highly intelligent marine mammals and are part of the toothed whales family. They are known for their playful behavior and complex communication skills.",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Bottlenose_Dolphin_%28South_Carolina_Aquarium%29.jpg/800px-Bottlenose_Dolphin_%28South_Carolina_Aquarium%29.jpg"
+    },
 ];
